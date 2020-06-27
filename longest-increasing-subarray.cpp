31a1679cf8365cbe9e9493992b3606bed431387e
Vasu Goel <goelvasu0712@gmail.com>
2020-06-28 01:08:02 +0530

    Longest Increasing Subarray

---
+++ longest-increasing-subarray.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// O(n) time, O(1) space
+vector<int> longestIncreasingSubarray(vector<int> nums) {
+    int n = nums.size(), end = 0, len = 1, maxlen = 1;
+
+    for(int i = 1; i < n; i++) {
+        if(nums[i] > nums[i-1])     len++;
+        else    len = 1;
+
+        if(len > maxlen) {
+            maxlen = len;
+            end = i;
+        }
+        cout << len << " " << maxlen << " " << end << endl;
+    }
+    return {nums.begin()+end-maxlen+1, nums.begin()+end+1};
+}
+
+int main() {
+    vector<int> nums {2, 1, 2, 5, 7, 9, 1, 4};
+
+    for(int x: longestIncreasingSubarray(nums))     cout << x << " "; cout << endl;
+    return 0;
+}
+
+/*
+
+nums:   2  1  2  5  7  9  1  4
+len:    1  1  2  3  4  5  1  2
+maxlen: 1  1  2  3  4  5  5  5
+end:    0  0  2  3  4  5  5  5
+
+*/
